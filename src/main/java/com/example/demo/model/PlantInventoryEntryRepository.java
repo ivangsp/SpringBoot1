b68f57fec4ac0bd1389ae37775c266727e76eef4
ivangsp <talk2gspivan@outlook.com>
2018-02-13 01:48:06 +0200

    project structure

---
+++ src/main/java/com/example/demo/model/PlantInventoryEntryRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long> {
+
+}
